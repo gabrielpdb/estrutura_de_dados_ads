5e405b803a93824139df31a722737ece08cd68d0
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2025-11-21 23:21:21 -0300

    separei os algoritmos em um arquivo cada

---
+++ complexidade/linear_search.c
@@ -0,0 +1,12 @@
+// O(n)
+int linear_search(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
---
+++ complexidade/triple_loop.c
@@ -0,0 +1,14 @@
+// O(n³)
+void triple_loop(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                int x = i + j + k;
+            }
+        }
+    }
+}
